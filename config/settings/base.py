44a1f6c23e50e27f65a559905a8b77856d78be3b
Anthony Addae <maaddae.co@gmail.com>
2023-01-06 22:53:43 +0000

    Create spreadsheets app

--- config/settings/base.py
+++ config/settings/base.py
@@ -79,6 +79,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "app.users",
+    "app.spreadsheets"
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
