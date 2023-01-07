502ecaccb792f9ad6e21ccccbe8ea9e9deb0706b
Anthony Addae <maaddae.co@gmail.com>
2023-01-07 20:10:53 +0000

    Create item model

--- app/spreadsheets/models.py
+++ app/spreadsheets/models.py
@@ -1,3 +1,15 @@
-from django.db import models  # noqa F401
+from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    description = models.TextField(blank=True)
+    image = models.URLField(blank=True, max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
