791f34b58f43ec5924315df13eb9e181505378ae
Anthony Addae <maaddae.co@gmail.com>
2023-01-11 02:29:34 +0000

    Modify list items view

--- app/spreadsheets/views.py
+++ app/spreadsheets/views.py
@@ -1,8 +1,6 @@
-from django.conf import settings
 from django.core.cache import cache
 from rest_framework import filters, generics
 
-from .models import Item
 from .serializers.spreadsheet import ItemListSerializer
 
 
@@ -17,8 +15,3 @@ class ItemListView(generics.ListAPIView):
         if "items" in cache:
             # get results from cache
             return cache.get("items")
-        else:
-            results = Item.objects.all()
-            # store data in cache
-            cache.set("items", results, timeout=settings.CACHE_TTL)
-            return results
